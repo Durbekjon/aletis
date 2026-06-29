4be96074fe733073b45724e1d2426d517b7eb563
Durbek Saydaliyev <saydaliyevdurbek0512@gmail.com>
2026-06-29 23:47:00 +0500

    feat(messages): conversation messages

---
+++ backend/src/modules/messages/messages.service.ts
@@ -0,0 +1,30 @@
+import { PrismaService } from '@core/prisma/prisma.service';
+import { Injectable } from '@nestjs/common';
+import { Message } from '@prisma/client';
+
+@Injectable()
+export class MessagesService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async _getCustomerLastMessages(
+    customerId: number,
+    limit: number = 10,
+  ): Promise<Message[]> {
+    return this.prisma.message.findMany({
+      where: { customerId },
+      orderBy: { createdAt: 'desc' },
+      take: limit,
+    });
+  }
+
+  async _saveMessage(
+    customerId: number,
+    content: string,
+    sender: 'USER' | 'BOT',
+    botId: number,
+  ): Promise<Message> {
+    return this.prisma.message.create({
+      data: { customerId, content, sender, botId },
+    });
+  }
+}
