0d2873da8a5c2b3295062e517a2be4738acb7111
Durbek Saydaliyev <saydaliyevdurbek0512@gmail.com>
2026-06-30 01:22:16 +0500

    feat(posts): post composer and scheduling

---
+++ backend/src/modules/posts/dto/schedule-post.dto.ts
@@ -0,0 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsISO8601 } from 'class-validator';
+
+export class SchedulePostDto {
+  @ApiProperty({ description: 'Schedule time in ISO format (UTC)' })
+  @IsISO8601()
+  scheduledAt: string;
+}
